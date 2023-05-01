2daba0fbea27bcbd46079c5620140d91552b9e1f
amirzmi <131792423+amirzmi@users.noreply.github.com>
2023-05-02 01:08:54 +0800

    Update apps.py

--- analyserApp/apps.py
+++ analyserApp/apps.py
@@ -7,7 +7,7 @@ class AnalyserappConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'analyserApp'
 
-    path = os.path.join(settings.MODELS,'models.p')
+    path = os.path.join(settings.MODELS,'SRmodels.p')
 
     with open(path, 'rb') as pickled:
         data = pickle.load(pickled)
